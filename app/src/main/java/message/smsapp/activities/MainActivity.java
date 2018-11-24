c9483e509f7439b59ac0e6c2c81bb2a2448bf578
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-11-24 20:56:14 +0100

    Refactoring code

--- app/src/main/java/message/smsapp/activities/MainActivity.java
+++ app/src/main/java/message/smsapp/activities/MainActivity.java
@@ -17,88 +17,88 @@ import message.smsapp.R;
 import message.smsapp.message.ConversationManager;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    private ConversationManager conversations;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        TextView title = findViewById(R.id.textView);
-        title.setText(getString(R.string.message_title));
-
-        ListView mainlist = findViewById(R.id.list_Messages);
-
-        this.conversations = ConversationManager.getInstance(this, mainlist);
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+		implements NavigationView.OnNavigationItemSelectedListener {
+	private ConversationManager conversations;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.activity_main);
+		
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		toggle.syncState();
+		
+		TextView title = findViewById(R.id.textView);
+		title.setText(getString(R.string.message_title));
+		
+		ListView mainlist = findViewById(R.id.list_Messages);
+		
+		this.conversations = ConversationManager.getInstance(this, mainlist);
+		
+		NavigationView navigationView = findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(this);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public boolean onNavigationItemSelected(MenuItem item) {
+		// Handle navigation view item clicks here.
+		int id = item.getItemId();
+		
+		if (id == R.id.nav_camera) {
+			// Handle the camera action
+		} else if (id == R.id.nav_gallery) {
+		
+		} else if (id == R.id.nav_slideshow) {
+		
+		} else if (id == R.id.nav_manage) {
+		
+		} else if (id == R.id.nav_share) {
+		
+		} else if (id == R.id.nav_send) {
+		
+		}
+		
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		drawer.closeDrawer(GravityCompat.START);
+		return true;
+	}
 }
--- app/src/main/java/message/smsapp/message/ConversationManager.java
+++ app/src/main/java/message/smsapp/message/ConversationManager.java
@@ -5,45 +5,52 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import message.smsapp.R;
 import message.smsapp.layouts_manager.MessagesAdapter;
 
 public class ConversationManager {
-    private static ConversationManager instance;
-
-    ArrayList<String> listOfComponents, listOfContents;
-    ArrayList<Integer> listOfIcons;
-
-    private ConversationManager(Context context, ListView list){
-        this.listOfComponents = new ArrayList<>();
-        this.listOfContents = new ArrayList<>();
-        this.listOfIcons = new ArrayList<>();
-
-        listOfComponents.add("Expéditeur 1");
-        listOfComponents.add("Expéditeur 2");
-
-        listOfContents.add("test1");
-        listOfContents.add("test2");
-
-        listOfIcons.add(R.drawable.ic_menu_send);
-        listOfIcons.add(R.drawable.ic_menu_share);
-
-        ListAdapter adapter = new MessagesAdapter(context, listOfComponents, listOfContents, listOfIcons);
-        list.setAdapter(adapter);
-    }
-
-    public static ConversationManager getInstance(Context context, ListView list){
-        if (ConversationManager.instance == null) {
-            return ConversationManager.instance = new ConversationManager(context, list);
-        } else {
-            return ConversationManager.instance;
-        }
-    }
-
-    private void addConversation(String title, String content, int id) {
-        this.listOfComponents.add(title);
-        this.listOfContents.add(content);
-        this.listOfIcons.add(id);
-    }
+	private static final int REQUEST_PERMISSION_KEY = 1;
+	
+	private static ConversationManager instance;
+	
+	private ArrayList<String> listOfComponents, listOfContents;
+	private ArrayList<Integer> listOfIcons;
+	private ArrayList<HashMap<String, String>> smsList;
+	
+	private ConversationManager(Context context, ListView list) {
+		this.listOfComponents = new ArrayList<>();
+		this.listOfContents = new ArrayList<>();
+		this.listOfIcons = new ArrayList<>();
+		this.smsList = new ArrayList<>();
+		
+		this.smsList.clear();
+		
+		listOfComponents.add("Expéditeur 1");
+		listOfComponents.add("Expéditeur 2");
+		
+		listOfContents.add("test1");
+		listOfContents.add("test2");
+		
+		listOfIcons.add(R.drawable.ic_menu_send);
+		listOfIcons.add(R.drawable.ic_menu_share);
+		
+		ListAdapter adapter = new MessagesAdapter(context, listOfComponents, listOfContents, listOfIcons);
+		list.setAdapter(adapter);
+	}
+	
+	public static ConversationManager getInstance(Context context, ListView list) {
+		if (ConversationManager.instance == null) {
+			return ConversationManager.instance = new ConversationManager(context, list);
+		} else {
+			return ConversationManager.instance;
+		}
+	}
+	
+	private void addConversation(String title, String content, int id) {
+		this.listOfComponents.add(title);
+		this.listOfContents.add(content);
+		this.listOfIcons.add(id);
+	}
 }
